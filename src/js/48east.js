05f83d9252939fab25e0f702dc0993736d134c8f
Rakesh sharma <rakeshsuthar99@gmail.com>
2017-05-31 14:05:25 +0530

    latest source with bugs fixes done

--- src/js/48east.js
+++ src/js/48east.js
@@ -2,41 +2,67 @@
 $(document).ready(function(){
   // sticky header
   $('#es-sticky-header-main').scrollToFixed({
-    maxWidth:768,
+    maxWidth:767,
     zIndex:11
   });
-  $('#es-sticky-header').scrollToFixed({
-    marginTop: 100,
-    zIndex: 10,
-    postFixed: function() {
-      $('#cart-btn').css({'opacity':'1'}).removeClass('sticky');
-      $('.filter').css('right', '20px');
-    },
-    preFixed: function() {
-      $('#cart-btn').css({'opacity':'0'}).addClass('sticky');
-      $('.filter').css('right', '140px');
+
+  $(window).resize(function(){
+    if($(this).width() >= 768){
+      $('#es-sticky-header').scrollToFixed({
+        marginTop: 0,
+        zIndex: 10,
+        postFixed: function() {
+          $('#cart-btn').css({'opacity':'1'}).removeClass('sticky');
+          $('.filter').css('right', '20px');
+        },
+        preFixed: function() {
+          $('#cart-btn').css({'opacity':'0'}).addClass('sticky');
+          $('.filter').css('right', '140px');
+        }
+      });
+    } else if($(this).width() <= 767) {
+      $('#es-sticky-header').scrollToFixed({
+        marginTop: 117,
+      });
+    }else {
+      $('#es-sticky-header').remove('scrollToFixed');
+
     }
-  });
+  })
+  .resize();//trigger resize on page load
+
+  // Unwanted elem clear
+  // $('.scroll-to-fixed-fixed').next().remove();
 
   // Location input visible
-  $('.location-input .location-selected').on('click', function(){
+  $('.location-input .location-selected').on('click touchstart', function(){
     $(this).hide();
     $(this).siblings('input').focus();
-    // $('.body-overlay').show();
-    // $('#es-sticky-header-main').css({'z-index': '5'});
-    // $('body').css({'overflow': 'hidden'});
+    $('.body-overlay').show();
+    $('body').css({'overflow': 'hidden'});
   });
+
   // On press enter on input
   $('.location-input input').keypress(function(e)
   {
+    var $this = $(this);
+    $(this).siblings('.suggestions').slideDown();
+
     if (e.keyCode == '13') {
       $(this).siblings('.location-selected').find('span').text($(this).val());
       $(this).siblings('.location-selected').show();
-      // $('.body-overlay').hide();
-      // $('#es-sticky-header-main').css({'z-index': '11'});
-      // $('body').css({'overflow': ''});
+      $('.body-overlay').hide();
+
       $(this).blur();
+      $(this).siblings('.suggestions').slideUp();
+      $('body').removeAttr('style');
     }
+
+    $($this).siblings('.suggestions').find('li').on('click', function(){
+      var content= $(this).text();
+      $($this).val(content);
+    });
+
   });
   // end
 
